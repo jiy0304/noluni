c7bb73178cddd08cd5339111065b726122027424
inyoung jeong <48130766+jiy0304@users.noreply.github.com>
2025-09-03 12:14:13 +0900

    Add files via upload

---
+++ src/component/Tablist1.jsx
@@ -0,0 +1,14 @@
+const Tablist1 = () => {
+  return (
+    <>
+      <ul>
+        <li><a href="#"><img src="../tab1_01.gif" alt="" /></a></li>
+        <li><a href="#"><img src="../tab1_02.gif" alt="" /></a></li>
+        <li><a href="#"><img src="../tab1_03.gif" alt="" /></a></li>
+        <li><a href="#"><img src="../tab1_04.gif" alt="" /></a></li>
+      </ul>
+    </>
+  )
+}
+
+export default Tablist1
---
+++ src/component/Tablist3.jsx
@@ -0,0 +1,14 @@
+const Tablist3 = () => {
+  return (
+    <>
+      <ul>
+        <li><a href="#"><img src="../tab3_01.gif" alt="" /></a></li>
+        <li><a href="#"><img src="../tab3_02.gif" alt="" /></a></li>
+        <li><a href="#"><img src="../tab3_03.gif" alt="" /></a></li>
+        <li><a href="#"><img src="../tab3_04.gif" alt="" /></a></li>
+      </ul>
+    </>
+  )
+}
+
+export default Tablist3
